46c799bc7f0563c06c78895da809ae6d6418c1af
archishma <askav@unc.edu>
2023-12-14 14:00:29 -0500

    work on getting g4s from transcripts

--- 231213_G4sFromTx.R
+++ 231213_G4sFromTx.R
@@ -2,6 +2,7 @@
 # I have all transcripts that contain at least 1 G4 
 # I also have start/stop coords for all exons comprising transcripts
 library(dplyr)
+library(tidyr)
 
 # load data 
 setwd("~/rna/autoregulation/g4/")
@@ -9,35 +10,101 @@ cds.exons <- read.table("GRCh38.cds.exons.tsv",
                         header = TRUE)
 cds.tx.g4 <- read.table("GRCh38.cds.txseqs.g4.tsv", 
                         col.names = c("id", "seq"))
+sum(is.na(cds.exons)) # 0
+sum(is.na(cds.tx.g4)) # 0
 
 utr5.exons <- read.table("GRCh38.utr5.exons.tsv", 
                          header = TRUE)
 utr5.tx.g4 <- read.table("GRCh38.utr5.txseqs.g4.tsv", 
                          col.names = c("id", "seq"))
+sum(is.na(utr5.exons)) # 0 
+sum(is.na(utr5.tx.g4)) # 0 
 
 utr3.exons <- read.table("GRCh38.utr3.exons.tsv", 
                          header = TRUE)
 utr3.tx.g4 <- read.table("GRCh38.utr3.txseqs.g4.tsv", 
                          col.names = c("id",  "seq"))
+sum(is.na(utr3.exons)) # 0 
+sum(is.na(utr3.tx.g4)) # 0
 
 # include exons corresponding to G4-containing transcripts 
 cds.g4.exons <- cds.exons %>% subset(id %in% cds.tx.g4$id)
 utr5.g4.exons <- utr5.exons %>% subset(id %in% utr5.tx.g4$id)
 utr3.g4.exons <- utr3.exons %>% subset(id %in% utr3.tx.g4$id)
 
-# get rid of objs we won't need anymore 
-rm(cds.exons)
-rm(utr5.exons)
-rm(utr3.exons)
+sum(is.na(cds.g4.exons)) # 0 
+sum(is.na(utr5.g4.exons)) # 0 
+sum(is.na(utr3.g4.exons)) # 0 
 
-# trying out grep in R on "ENST00000005340"
+# function to split the ranges and account for 1-length sequences 
+# TODO: optimize later... im using grepl
+split.range <- function(string, num = c(1,2)) {
+  if (grepl("-", string)) {
+    return(strsplit(string, split = "-")[[1]][num])
+  }
+  else {
+    return(string)
+  }
+}
+
+### split up chr, start_pos, stop_pos, strand 
+# CDS 
+cds.tx.exons <- cds.g4.exons %>% 
+  separate(col = "annot", into = c("chr", "range", "strand"), sep = ":") 
+sum(is.na(cds.tx.exons)) # 0
+
+cds.tx.exons$starts <- cds.tx.exons$range %>% lapply(split.range, num = 1)
+cds.tx.exons$stops <- cds.tx.exons$range %>% lapply(split.range, num = 2)
+sum(is.na(cds.tx.exons)) # 0. this is not the behavior I get with separate()
+
+# 5' UTR
+utr5.tx.exons <- utr5.g4.exons %>%  
+  separate(col = "annot", into = c("chr", "range", "strand"), sep = ":")
+utr5.tx.exons$starts <- utr5.tx.exons$range %>% lapply(split.range, num = 1)
+utr5.tx.exons$stops <- utr5.tx.exons$range %>% lapply(split.range, num = 2)
+sum(is.na(utr5.tx.exons)) # 0 
+
+# 3' UTR
+utr3.tx.exons <- utr3.g4.exons %>% 
+  separate(col = "annot", into = c("chr", "range", "strand"), sep = ":")
+utr3.tx.exons$starts <- utr3.tx.exons$range %>% lapply(split.range, num = 1)
+utr3.tx.exons$stops <- utr3.tx.exons$range %>% lapply(split.range, num = 2)
+
+# get rid of objs we won't need anymore 
+rm(cds.exons, cds.g4.exons)
+rm(utr5.exons, utr5.g4.exons)
+rm(utr3.exons, utr3.g4.exons)
+
+### make sure that didn't give me NA values 
+
+### TESTING
+### trying out grep in R on "ENST00000005340"
 example <- cds.tx.g4[cds.tx.g4$id == "ENST00000005340",]
 # I know (visually) that 15 exons comprise this transcript 
 seq <- example$seq
 match <- seq %>% gregexpr(pattern = "[G]{3,5}.{1,7}[G]{3,5}.{1,7}[G]{3,5}.{1,7}[G]{3,5}.{1,7}")
-match
-poss <- seq %>% substr(start = 1795, stop = 1795 + 28 - 1)
-nchar(poss)
-poss
+# https://stackoverflow.com/questions/51875360/accessing-results-of-gregexpr
+# this works because there is only one text element: seq
+# but I could pass in a list and iterate over match[[1]] ... match[[n]]
+starts <- match[[1]]
+attributes(starts) <- NULL
+lengths <- attr(match[[1]], 'match.length')
+starts
+lengths
 
-# where is it in the exons 
+s1 <- 0
+s2 <- 0
+for (i in length(starts)) {
+  s1 <- starts[i]
+  s2 <- starts[i] + lengths[i] - 1
+  poss <- seq %>% substr(start = starts[i], stop = starts[i] + lengths[i] - 1)
+  print(nchar(poss))
+  print(poss)
+}
+
+# where is it in the transcript. 1-based indexing 
+s1
+s2
+# get exons corresponding to transcript 
+exons.5340 <- cds.tx.exons[cds.tx.exons$id == "ENST00000005340",]
+exons.5340
